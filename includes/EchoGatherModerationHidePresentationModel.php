a811c307abb463db66255cc7f3d795224067109a
Matthew Flaschen <mflaschen@wikimedia.org>
2015-12-03 01:36:25 -0500

    Change Echo notifications to use new formatter with presentation model
    Bug: T116848
    Change-Id: I9990813c76c734e52190bbf66e3a99c38a9872eb

---
+++ includes/EchoGatherModerationHidePresentationModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Gather;
+
+use Title;
+
+class EchoGatherModerationHidePresentationModel extends EchoGatherModerationPresentationModel {
+	public function getSecondaryLinks() {
+		$criteriaTitle = Title::newFromText(
+			$this->msg( 'gather-moderation-criteria-page' )->inContentLanguage()->text()
+		);
+
+		$contestTitle = Title::newFromText(
+			$this->msg( 'gather-moderation-dispute-page' )->inContentLanguage()->text()
+		);
+
+		return array(
+			array(
+				'url' => $criteriaTitle->getFullURL(),
+				'label' => $this->msg( 'gather-moderation-criteria-link-label' )->text(),
+				'description' => $this->msg( 'gather-moderation-criteria-link-description' )->text(),
+				'icon' => false,
+				'prioritized' => true,
+			),
+			array(
+				'url' => $contestTitle->getFullURL(),
+				'label' => $this->msg( 'gather-moderation-dispute-link-label' )->text(),
+				'description' => $this->msg( 'gather-moderation-dispute-link-description' )->text(),
+				'icon' => false,
+
+				// This could be made false, depending how much visibility this option requires.
+				// It could also be linked from the criteria page.
+				'prioritized' => true,
+			),
+		);
+	}
+}
